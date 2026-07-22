31d19234d5289ce4b717bba273fed71b3e0e9fb9
oiuwerjksdk <sobelotokuche@protonmail.com>
2026-07-22 18:12:45 +0200

    Add files via upload

--- server.js
+++ server.js
@@ -1,14 +1,26 @@
-const fs = require('fs')
 const axios = require('axios')
 const express = require('express')
 const app = express()
-let database = require('./database.json')
+let db = [
+  "krissone",
+  "sabrinajadex",
+  "nicole_anistonn",
+  "bad__princess",
+  "devyale",
+  "belle_emmanuelle",
+  "sabi_wii",
+  "tastypoint",
+  "_mariarty_",
+  "nadeen_",
+  "ellediane",
+  "kim_red_"
+]
 
 
 app.get('/', (req, res) => routeIndex(req, res))
+app.get('/db', (req, res) => res.send(db))
 app.get('/add', (req, res) => routeAdd(req, res))
 app.get('/remove', (req, res) => routeRemove(req, res))
-app.get('/database', (req, res) => res.send(database))
 
 
 app.listen(3000, () => console.log(`Server is running on port 3000...`))
@@ -16,7 +28,7 @@ app.listen(3000, () => console.log(`Server is running on port 3000...`))
 
 function routeIndex(req, res) {
   let html = ''
-  database.forEach(el => {
+  db.forEach(el => {
     html +=
       `<img src="https://jpeg.live.mmcdn.com/stream?room=${el}&f=${Math.random()}" alt="${el}" width="280" height="160" onclick="window.open('https://chaturbate.com/${el}','_blank', 'noopener,noreferrer')">`
   })
@@ -25,15 +37,13 @@ function routeIndex(req, res) {
 
 
 function routeAdd(req, res) {
-  database.unshift(req.query.name)
-  fs.writeFileSync('./database.json', JSON.stringify([...new Set(database)]))
+  db.unshift(req.query.name)
   res.send('added')
 }
 
 
 function routeRemove(req, res) {
-  database = database.filter(el => el !== req.query.name)
-  fs.writeFileSync('./database.json', JSON.stringify(database))
+  db = db.filter(el => el !== req.query.name)
   res.send('removed')
 }
 
